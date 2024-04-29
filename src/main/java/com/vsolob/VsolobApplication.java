85beb1ab112247857a6f4f7f9d7f5289446a5f47
vedatbilaloglu <vedatbilaloglu@hotmail.com>
2024-04-29 22:28:41 +0300

    init

---
+++ src/main/java/com/vsolob/VsolobApplication.java
@@ -0,0 +1,13 @@
+package com.vsolob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsolobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsolobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vsolob/VsolobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsolob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsolobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
